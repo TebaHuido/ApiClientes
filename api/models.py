515d860d256a0314663032f8f3a16ecf12b8a1e5
TebaHuido <baase.hui@gmail.com>
2023-04-02 21:46:48 -0400

    m2m update

--- api/models.py
+++ api/models.py
@@ -11,14 +11,20 @@ def permission_default():
 class Permission(models.Model):
 	description=models.CharField(max_length=500)
 	permname=models.CharField(max_length=200)
+	def __unicode__(self):
+		return self.permname
 
 class Profile(models.Model):
 	pname=models.CharField(max_length=200)
-	permission=models.ManyToManyField(Permission,null=False,  default=permission_default)
+	permission=models.ManyToManyField(Permission)
+	def __unicode__(self):
+		return self.pname
 
 class User(models.Model):
 	uname=models.CharField(max_length=200)
 	password=models.CharField(max_length=200)
 	mail=models.CharField(max_length=200)
 	rut=models.CharField(max_length=11)
-	profile=models.ManyToManyField(Profile , null=False , blank=False, default=profile_default)
+	profile=models.ManyToManyField(Profile)
+	def __unicode__(self):
+		return self.uname
